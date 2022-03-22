3ac59e8899fbd54c2a2ad6f8cb0aac7d337ae64c
Olivier Hoareau <oha@greenberets.io>
2022-03-22 22:53:07 +0100

    fix: fix

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   layers = {for k,v in var.layers: k => {
     name          = "${var.env}-${k}"
     bucket        = var.s3_bucket
-    key           = "${var.s3_prefix != null ? var.s3_prefix : ""}${var.env}-layer-${k}.zip"
+    key           = "${var.s3_prefix != null ? var.s3_prefix : ""}${var.env}-${k}.zip"
     license       = "ISC"
     runtimes      = v.runtimes
     architectures = v.architectures
